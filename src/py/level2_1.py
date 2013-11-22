6c63b7bc3bc5e02f5fe78c238885b289f0d0f21a
Patrick M. Niedzielski <PatrickNiedzielski@gmail.com>
2013-11-22 20:22:33 +0000

    Forgot to defive at_end()

--- src/py/level2_1.py
+++ src/py/level2_1.py
@@ -18,3 +18,6 @@ player1._position = [2, 2];
 _world.grid[player1._position[1]][player1._position[0]] = player1
 player2._position = [14, 6];
 _world.grid[player2._position[1]][player2._position[0]] = player2
+
+def at_end():
+    # nothing
