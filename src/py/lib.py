6d1ab030d265aa5f5d7cd4fdd0572384417a63bc
Patrick M. Niedzielski <PatrickNiedzielski@gmail.com>
2013-11-22 19:53:58 +0000

    Fix to the JSON for talking.

--- src/py/lib.py
+++ src/py/lib.py
@@ -239,7 +239,7 @@ class Player:
         exec script
 
     def say(self, message):
-        print str([self._name, message]);
+        print "[\"" + str(self._name) + "\", \"" + message + "\"]");
         self._messages.append(message)
         gevent.sleep(0)
 
