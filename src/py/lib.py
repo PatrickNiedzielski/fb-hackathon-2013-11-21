a7498e4765190f75cba452782b6bc2577a131027
Patrick M. Niedzielski <PatrickNiedzielski@gmail.com>
2013-11-22 20:15:25 +0000

    Fix to drop wall.

--- src/py/lib.py
+++ src/py/lib.py
@@ -199,10 +199,10 @@ class Player:
             raise Exception("Outside Map")
         if (_world.grid[temppos[1]][temppos[0]] != None):
             raise Exception("Collision")
-            if _dropWall:
-                _world.grid[self._position[1]][self._position[0]] = Wall([self._position[1]][self._position[0]])
-            else:
-                _world.grid[self._position[1]][self._position[0]] = None
+        if _dropWall:
+            _world.grid[self._position[1]][self._position[0]] = Wall([self._position[1]][self._position[0]])
+        else:
+            _world.grid[self._position[1]][self._position[0]] = None
         self._position = temppos
         _world.grid[self._position[1]][self._position[0]] = self
         self._turn_dir(dir);
