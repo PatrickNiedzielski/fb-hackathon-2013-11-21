c41655cd58f9c7de8fe7c06634c9b358ddebf423
root <root@ip-10-226-146-94.us-west-2.compute.internal>
2013-11-22 16:30:58 +0000

    Changes

--- src/py/tmp.py
+++ src/py/tmp.py
@@ -155,3 +155,65 @@ class Player:
 player1 = Player(1, [1, 0])
 player2 = Player(2, [0, 1], player1)
 player1.other = player2
+# Learn Python -- level 1 logic
+# Copyright (C) 2013 Cornell FB Hackathon Team.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+player1._position = [5, 6];
+player2._position = [14, 6];
+
+def at_end():
+    if (len(player1._messages) == 0 or \
+        len(player2._messages) == 0):
+        raise Exception("Failure")
+# Learn Python -- script loading and running
+# Copyright (C) 2013 Cornell FB Hackathon Team.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+print _sub_none(repr(_world.grid))
+
+_script1_length = input()
+_script1 = ""
+for i in range(_script1_length):
+    _script1 += raw_input() + "\n"
+_script2_length = input()
+_script2 = ""
+for i in range(_script2_length):
+    _script2 += raw_input() + "\n"
+
+def run_script1():
+    exec _script1
+
+def run_script2():
+    exec _script2
+
+gevent.joinall([
+    gevent.spawn(lambda: Player._run_script(player1, _script1)),
+    gevent.spawn(lambda: Player._run_script(player2, _script2)),
+])
+
+at_end()
