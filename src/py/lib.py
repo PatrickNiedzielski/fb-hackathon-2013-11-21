a17d3e3ca7c2fc20985d0f37a5ed388f0df1c553
Patrick M. Niedzielski <PatrickNiedzielski@gmail.com>
2013-11-22 10:07:48 +0000

    Refactor code for loading and running the scripts out of the lib.py file into separate file.  Will be concatenated in the Javascript loader.

--- src/py/lib.py
+++ src/py/lib.py
@@ -1,4 +1,18 @@
-#!/usr/bin/env python
+# Learn Python -- library functions
+# Copyright (C) 2013 Cornell FB Hackathon Team.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 import gevent
 
@@ -117,24 +131,3 @@ player1 = Player(1, [1, 0])
 player2 = Player(2, [0, 1], player1)
 player1.other = player2
 print str(_world.grid)
-
-
-_script1_length = input()
-_script1 = ""
-for i in range(_script1_length):
-    _script1 += raw_input() + "\n"
-_script2_length = input()
-_script2 = ""
-for i in range(_script2_length):
-    _script2 += raw_input() + "\n"
-
-def run_script1():
-    exec _script1
-
-def run_script2():
-    exec _script2
-
-gevent.joinall([
-    gevent.spawn(lambda: Player._run_script(player1, _script1)),
-    gevent.spawn(lambda: Player._run_script(player2, _script2)),
-])
